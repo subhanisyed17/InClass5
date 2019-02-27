ed137c797273b2dbbe31e17f5e4e16edaf9c3a57
subhanisyed17@gmail.com <subhanisyed17@gmail.com>
2019-02-27 08:52:20 +0530

    sql-queries

---
+++ dbo.Products.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Products] (
+    [ProductId]   INT          NOT NULL,
+    [ProductName] VARCHAR (50) NULL,
+    CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED ([ProductId] ASC)
+);
+
---
+++ dbo.Sales.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Sales] (
+    [Id]        INT NOT NULL,
+    [ProductId] INT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_ProductId] FOREIGN KEY ([ProductId]) REFERENCES [dbo].[Products] ([ProductId])
+);
+
